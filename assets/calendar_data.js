77b5e5be297343199baa85b548bc3284f3d3b425
quinteroer <quinteroer@vcu.edu>
2026-02-16 09:13:40 -0500

    Update calendar_data.js

--- assets/calendar_data.js
+++ assets/calendar_data.js
@@ -29,7 +29,7 @@ const loveData = {
   },
   "day3": {
     "title": "Day 3",
-    "message": "I can't help but think how enchanted youve madr my life. I love you so much forever and ever.",
+    "message": "I can't help but think how enchanted you've made my life. I love you so much forever and ever.",
     "src": "https://embed.music.apple.com/us/song/enchanted-taylors-version/1690840535",
     "song_embed": "<iframe allow=\"autoplay *; encrypted-media *; fullscreen *; clipboard-write\" frameborder=\"0\" height=\"175\" style=\"width:100%;max-width:660px;overflow:hidden;border-radius:10px;\" sandbox=\"allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation\" src=\"https://embed.music.apple.com/us/song/enchanted-taylors-version/1690840535\"></iframe>",
     "PID": "5669B1B05268E853",
